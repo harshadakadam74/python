8e73c0a26f4ce10a6bf5bd5aa195a4ee1f6062ba
harshadakadam74 <kadamharshada@gmail.com>
2026-07-07 15:24:28 +0530

    practice python zero to advance

---
+++ 2_comment_escopeSequence/one.py
@@ -0,0 +1,21 @@
+#Comment one line comment
+# Escape sequence
+'''
+multiple 
+line 
+comment
+'''
+
+
+# \n - new line
+print("Hello Welcome to the \n world of python programming!")
+
+# use double "" and single '' of string
+print("Hello \"Welcome\" to the \n world of python programming!")
+print("Hello Welcome\' to the \n world of python programming!")
+
+
+# multiple use of escape sequence
+#sep= Separator
+print("Heyy" ,1,2,3, sep="~", end="009\n")
+print("Hiiii")
