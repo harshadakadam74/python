55ef2c68f9315ab02769369996860d54527c5294
harshadakadam74 <kadamharshada@gmail.com>
2026-07-07 21:09:43 +0530

    String function in python

---
+++ String/one.py
@@ -0,0 +1,17 @@
+name = "Kadam"
+# use ''' is multiline string
+apple = ''' lorem ipsum dolor sit amet, 
+consectetur adipiscing elit.
+Lorem ipsum /"dolor/" sit amet, consectetur adipiscing elit.'''
+
+print("Hello", name)
+print(apple)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+for char in apple:
+    print(char)
