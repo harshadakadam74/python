7e58106edcbc95ced1522486c5d3e023b3a3576e
harshadakadam74 <kadamharshada@gmail.com>
2026-07-07 20:16:32 +0530

    typecasting in python

---
+++ Typecasting/one.py
@@ -0,0 +1,11 @@
+string = "15"
+number = 4
+string_number = int(string)
+sum = number + string_number
+print("the sum of both is number is:", sum)
+
+
+# implicit type casting
+c=1.9
+d=8
+print(c+d)
