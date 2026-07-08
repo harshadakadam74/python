c7b4aa12ce834c1fab2cf18770e3f79d4a171acb
harshadakadam74 <kadamharshada@gmail.com>
2026-07-08 09:54:24 +0530

    Commited in slicing

--- String/StringSlicing.py
+++ String/StringSlicing.py
@@ -2,12 +2,12 @@ fruit = "Mango"
 mongoLen = len(fruit)
 print(mongoLen)
 print(fruit[0:4]) #including 0 but not 4
-print(fruit[1:4])
-print(fruit[:5])
-print(fruit[0:-3])
-print(fruit[len(fruit)-3])
-print(fruit[-1:len(fruit)-3])
-print(fruit[-3:-1])
+print(fruit[1:4]) #including 1 but not 4
+print(fruit[:5]) #including 0 but not 5
+print(fruit[0:-3]) #including 0 but not -3
+print(fruit[len(fruit)-3]) #including the character at index (length - 3)
+print(fruit[-1:len(fruit)-3]) #including -1 but not (length - 3)
+print(fruit[-3:-1]) #including -3 but not -1
 
 pr = "Harry"
 print(pr[-4:-2])
