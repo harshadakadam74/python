049ed473ad0676eb8f38af41301dd2250b084ab0
harshadakadam74 <kadamharshada@gmail.com>
2026-07-08 09:53:33 +0530

    String Slicing in python

---
+++ String/StringSlicing.py
@@ -0,0 +1,13 @@
+fruit = "Mango"
+mongoLen = len(fruit)
+print(mongoLen)
+print(fruit[0:4]) #including 0 but not 4
+print(fruit[1:4])
+print(fruit[:5])
+print(fruit[0:-3])
+print(fruit[len(fruit)-3])
+print(fruit[-1:len(fruit)-3])
+print(fruit[-3:-1])
+
+pr = "Harry"
+print(pr[-4:-2])
