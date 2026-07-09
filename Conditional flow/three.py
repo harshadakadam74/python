e5c3e2fe9d46b576fb1a625932b2e77ad152e9be
harshadakadam74 <kadamharshada@gmail.com>
2026-07-09 19:35:30 +0530

    elif statement in python

---
+++ Conditional flow/three.py
@@ -0,0 +1,11 @@
+num = int(input("Enter the value if num: "))
+if(num < 0):
+    print("The number is negative")
+elif(num == 0):
+    print("The number is zero")
+elif(num == 999):
+    print("The number is special")
+else: 
+    print("The number is positive")
+    
+print("I am happy now")
