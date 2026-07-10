62613f70bab1e302b9c1fd120634ede7d38f97a5
harshadakadam74 <kadamharshada@gmail.com>
2026-07-10 17:03:13 +0530

    for loop in python

---
+++ LoopStatement/For01.py
@@ -0,0 +1,22 @@
+name = "Kadam"
+for i in name:
+    # print(i,end=",")
+    print(i)
+    if (i == "d"):
+        print("Break the loop")
+
+colors = ["Green", "Blue","Yellow","Pink"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+        
+for k in range(5):
+    print(k+1)
+    
+for k in range(100+1):
+    print(k)
+    
+for k in range(1,12,3):
+    print(k)
+    
