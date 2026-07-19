955ea5c2092abd16f2a7f011d1326e67f6bb64c5
harshadakadam74 <kadamharshada@gmail.com>
2026-07-19 22:15:33 +0530

    ElseLoop using in python

--- LoopStatement/ElseLoop.py
+++ LoopStatement/ElseLoop.py
@@ -28,11 +28,11 @@ else:
 # Python allows the else keyword to be used with the for and while loops too. The else block appears after the body of the loop. The statements in the else block will be executed after all iterations are completed. The program exits the loop only after the else block is executed.
 
 # Syntax
-for counter in sequence:
+# for counter in sequence:
     #Statements inside for loop block
-else:
+# else:
     #Statements inside else block
-Example:
+# Example:
 for x in range(5):
     print ("iteration no {} in for loop".format(x+1))
 else:
